783f0fe13c83e569ef5329269f53b690859af0e4
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-03-27 20:24:57 -0300

    Formatter changes

--- lexer/src/test/kotlin/LexerTest.kt
+++ lexer/src/test/kotlin/LexerTest.kt
@@ -1,18 +1,19 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import kotlin.test.Test
 
-class LexerTest{
+class LexerTest {
+    // "let a: string = 5 * 5;"
     @Test
-    //"let a: string = 5 * 5;"
-    fun `test 001 - should make a list of tokens`(){
+    fun `test 001 - should make a list of tokens`() {
         val example = "let a: string = 5 * 5;"
         val lexer = Lexer(example)
         val tokens = lexer.makeTokens()
         assertEquals(9, tokens.size)
     }
+
+    // "println(a);"
     @Test
-    //"println(a);"
-    fun `test 002 - should make a list of tokens`(){
+    fun `test 002 - should make a list of tokens`() {
         val example = "println(a);"
         val lexer = Lexer(example)
         val tokens = lexer.makeTokens()
--- parser/src/test/kotlin/ParserTest.kt
+++ parser/src/test/kotlin/ParserTest.kt
@@ -1,40 +1,42 @@
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class ParserTest{
+class ParserTest {
     @Test
-    fun `test 001 - should convert a list of tokens in ast`(){
-        val tokens : List<Token> = listOf(
-            Token(TokenType.LET_KEYWORD, "let", Position(1, 1), Position(3, 1)),
-            Token(TokenType.IDENTIFIER, "a", Position(5, 1), Position(5, 1)),
-            Token(TokenType.COLON, ":", Position(6, 1), Position(6, 1)),
-            Token(TokenType.STRING_TYPE, "string", Position(8, 1), Position(13, 1)),
-            Token(TokenType.EQ, "=", Position(15, 1), Position(15, 1)),
-            Token(TokenType.NUMBER, "5", Position(17, 1), Position(17, 1)),
-            Token(TokenType.TIMES, "*", Position(19, 1), Position(19, 1)),
-            Token(TokenType.NUMBER, "5", Position(21, 1), Position(21, 1)),
-            Token(TokenType.SEMICOLON, ";", Position(22, 1), Position(22, 1)),
-            Token(TokenType.PRINTLN_FUNCTION, "println", Position(24, 1), Position(30, 1)),
-            Token(TokenType.LPAREN, "(", Position(31, 1), Position(31, 1)),
-            Token(TokenType.IDENTIFIER, "a", Position(32, 1), Position(32, 1)),
-            Token(TokenType.RPAREN, ")", Position(33, 1), Position(33, 1)),
-            Token(TokenType.SEMICOLON, ";", Position(34, 1), Position(34, 1))
-        )
+    fun `test 001 - should convert a list of tokens in ast`() {
+        val tokens: List<Token> =
+            listOf(
+                Token(TokenType.LET_KEYWORD, "let", Position(1, 1), Position(3, 1)),
+                Token(TokenType.IDENTIFIER, "a", Position(5, 1), Position(5, 1)),
+                Token(TokenType.COLON, ":", Position(6, 1), Position(6, 1)),
+                Token(TokenType.STRING_TYPE, "string", Position(8, 1), Position(13, 1)),
+                Token(TokenType.EQ, "=", Position(15, 1), Position(15, 1)),
+                Token(TokenType.NUMBER, "5", Position(17, 1), Position(17, 1)),
+                Token(TokenType.TIMES, "*", Position(19, 1), Position(19, 1)),
+                Token(TokenType.NUMBER, "5", Position(21, 1), Position(21, 1)),
+                Token(TokenType.SEMICOLON, ";", Position(22, 1), Position(22, 1)),
+                Token(TokenType.PRINTLN_FUNCTION, "println", Position(24, 1), Position(30, 1)),
+                Token(TokenType.LPAREN, "(", Position(31, 1), Position(31, 1)),
+                Token(TokenType.IDENTIFIER, "a", Position(32, 1), Position(32, 1)),
+                Token(TokenType.RPAREN, ")", Position(33, 1), Position(33, 1)),
+                Token(TokenType.SEMICOLON, ";", Position(34, 1), Position(34, 1)),
+            )
 
         val parser = Parser(tokens)
         val actualAst = parser.generateAST()
 
-        val expectedAst = listOf(
-            DeclarationAssignation(
-                Declaration("a", "string"),
-                BinaryOperation(
-                    NumberOperator(5.0),
-                    "*",
-                    NumberOperator(5.0)
-                )
-            ),
-            Method("println", IdentifierOperator("a"))
-        )
+        val expectedAst =
+            listOf(
+                DeclarationAssignation(
+                    Declaration("a", "string"),
+                    BinaryOperation(
+                        NumberOperator(5.0),
+                        "*",
+                        NumberOperator(5.0),
+                    ),
+                ),
+                Method("println", IdentifierOperator("a")),
+            )
 
         assertEquals(expectedAst, actualAst)
     }
