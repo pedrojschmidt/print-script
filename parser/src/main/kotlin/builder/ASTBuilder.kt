4a426a67c04231229ef3b379a964bdf6dd7b043b
juancruzdelorenzo <juancruz.delorenzo@ing.austral.edu.ar>
2024-04-11 15:57:22 -0300

    Format code

--- parser/src/main/kotlin/builder/ASTBuilder.kt
+++ parser/src/main/kotlin/builder/ASTBuilder.kt
@@ -5,8 +5,8 @@ import Token
 
 interface ASTBuilder<out T : ASTNode> {
     // Method to verify if this builder can build the AST for the given tokens
-    fun verify(tokens: List<Token>): Boolean
+    fun verify(statement: List<Token>): Boolean
 
     // Method to actually build the AST for the given tokens
-    fun build(tokens: List<Token>): T
+    fun build(statement: List<Token>): T
 }
