42e0f937af8c49c74feaf9afc6f3e9128bfcc4f9
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-04-25 18:11:23 -0300

    CLI improvements

--- cli/src/main/kotlin/ExecuteCommand.kt
+++ cli/src/main/kotlin/ExecuteCommand.kt
@@ -1,71 +1,15 @@
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
 import java.io.File
 import java.io.FileInputStream
-import kotlin.math.ceil
 
 class ExecuteCommand(private val file: File, private val lexer: Lexer, private val parser: Parser, private val interpreter: Interpreter) : Command {
     override fun execute() {
         println("Executing...")
 
-        val astList = fillAstListWithProgress(file)
+        val tokenProvider = TokenProvider(FileInputStream(file))
+
+        val astList = fillAstListWithProgress(file, parser, tokenProvider)
         val result = interpreter.interpretAST(astList)
 
         println(result)
-    }
-
-    private fun fillAstListWithProgress(file: File): MutableList<ASTNode> {
-        val tokenProvider = TokenProvider(FileInputStream(file))
-        val astList = mutableListOf<ASTNode>()
-
-        val totalLines = file.readLines().size
-        val progressInterval = totalLines / 10 // Actualizar la barra de progreso cada 10%
-
-        var linesProcessed = 0
-        var progressCounter = 0
-
-        // Mutex para evitar condiciones de carrera al actualizar el progreso
-        val progressMutex = Mutex()
-
-        runBlocking {
-            launch(Dispatchers.IO) {
-                while (tokenProvider.hasNextStatement()) {
-                    val tokens = tokenProvider.readStatement()
-                    val ast = parser.generateAST(tokens)
-                    ast?.let {
-                        // Incrementar el contador de líneas procesadas
-                        linesProcessed++
-                        // Incrementar el contador de progreso
-                        progressCounter++
-                        // Actualizar la barra de progreso si es necesario
-                        if (progressCounter >= progressInterval || linesProcessed == totalLines) {
-                            // Bloquear el mutex antes de actualizar el progreso
-                            progressMutex.withLock {
-                                val progress = (linesProcessed.toDouble() / totalLines.toDouble()) * 100
-                                printProgress(progress)
-                                // Reiniciar el contador de progreso
-                                progressCounter = 0
-                            }
-                        }
-                        astList.add(it)
-                    }
-                }
-            }
-        }
-
-        return astList
-    }
-
-    private fun printProgress(progress: Double) {
-        // Limpiar la línea anterior antes de imprimir el progreso actualizado
-        print("\r" + " ".repeat(50) + "\r")
-        // Calcular el número de bloques llenos y vacíos para la barra de progreso
-        val numBlocksFilled = ceil(progress / 2).toInt()
-        val numBlocksEmpty = 50 - numBlocksFilled
-        // Imprimir la barra de progreso
-        print("[" + "#".repeat(numBlocksFilled) + "-".repeat(numBlocksEmpty) + "]  ${progress.toInt()}% \n")
     }
 }
--- cli/src/main/kotlin/Main.kt
+++ cli/src/main/kotlin/Main.kt
@@ -57,7 +57,7 @@ Version:
 
 Option"""
 
-    CLIaux(
+    CLI(
         CommandFactory(
             lexer,
             parser,
